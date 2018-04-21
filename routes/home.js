d825bdba42e7c786ac0edeb8031c51db7d55cb06
Rispolyv0n <navy.horizon.314@gmail.com>
2018-04-21 15:10:26 +0800

    modify routes/*.js variables to camel form and remove gulp in package.json

--- routes/home.js
+++ routes/home.js
@@ -5,20 +5,20 @@ const router = new express.Router();
 
 // route to root directory
 router.get( '/', function( req, res ) {
-    let obj_to_send = new Object();
+    let objToSend = new Object();
 
-    obj_to_send.announcement_id = req.query.announcement_id;
-    obj_to_send.tag = req.query.tag;
-    obj_to_send.time = req.query.time;
-    obj_to_send.announcement_data = 'ask db';
+    objToSend.announcement_id = req.query.announcement_id;
+    objToSend.tag = req.query.tag;
+    objToSend.time = req.query.time;
+    objToSend.announcement_data = 'ask db';
 
-    if(obj_to_send.announcement_id != undefined){
+    if(objToSend.announcement_id != undefined){
         // check if announcement_id is valid
-        res.render( 'announcement/detail', {announcement_id : obj_to_send.announcement_id} );
-    }else if(obj_to_send.tag != undefined || obj_to_send.time != undefined){
+        res.render( 'announcement/detail', {announcement_id : objToSend.announcement_id} );
+    }else if(objToSend.tag != undefined || objToSend.time != undefined){
         // check if tag is valid
         // check if time is valid, if time == undefined -> no time filter
-        res.render( 'search/search', obj_to_send );
+        res.render( 'search/search', objToSend );
     }else{
         res.render( 'home/index' );
     }
--- routes/student.js
+++ routes/student.js
@@ -5,34 +5,34 @@ const testDB = require ( './testDB' );
 
 const query_handler = ( page_name ) => {
     return function(req, res, next){
-        let obj_to_send = {page: req.query.page, annoucement_id: req.query.announcement_id, tag: req.query.tag, data_number: 0};
+        let objToSend = {page: req.query.page, annoucement_id: req.query.announcement_id, tag: req.query.tag, data_number: 0};
 
-        if(obj_to_send.page == undefined) obj_to_send.page = 1;
+        if(objToSend.page == undefined) objToSend.page = 1;
 
         // tag correspond.
         switch (page_name){
             case '/course':
-                obj_to_send.tag = 6;
+                objToSend.tag = 6;
                 break;
             case '/college':
-                obj_to_send.tag = 7;
+                objToSend.tag = 7;
                 break;
             case '/master':
-                obj_to_send.tag = 8;
+                objToSend.tag = 8;
                 break;
             case '/phd':
-				obj_to_send.tag = 9;
+				objToSend.tag = 9;
                 break;
             case '/scholarship':
-                obj_to_send.tag = 10;
+                objToSend.tag = 10;
                 break;
 			case '/international':
-				obj_to_send.tag =11;
+				objToSend.tag =11;
 				break;
         }
         let data = {data_number: 1, content: testDB.find()};
-        if(obj_to_send.announcement_id != undefined){
-            res.render( 'student/detail', { announcement_id: obj_to_send.announcement_id} );
+        if(objToSend.announcement_id != undefined){
+            res.render( 'student/detail', { announcement_id: objToSend.announcement_id} );
         }else{
             res.render( 'student/' + page_name, data);
         }
